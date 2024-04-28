face80f506b767ae93e8d5ba26dcd89d6cf8973f
ss-deep <deepalisangole@gmail.com>
2024-04-28 16:58:01 -0500

    solved

--- python/Majority Element.py
+++ python/Majority Element.py
@@ -4,7 +4,6 @@ eg : Input: nums = [3,2,3]
 Output: 3
 eg : Input: nums = [2,2,1,1,1,2,2]
 Output: 2'''
-import math
 def majorityElement(nums):
     """
     :type nums: List[int]
@@ -13,9 +12,9 @@ def majorityElement(nums):
     nums_dict={}
     for i in nums:
         # 2 - 1, 
-        # nums_dict[i]=nums_dict.get(i,0) + 1
-        # if nums_dict.get(i) > len(nums) / 2:
-        #     return i
+        nums_dict[i]=nums_dict.get(i,0) + 1
+        if nums_dict.get(i) > len(nums) / 2:
+            return i
 
         # if i in nums_dict:
         #     nums_dict[i]+=1
@@ -31,4 +30,3 @@ print(majorityElement([2,2,1,1,1,2,2]))
 print(majorityElement([6,5,5]))
 print(majorityElement([1]))
 
-print(math.ceil(0.5))
