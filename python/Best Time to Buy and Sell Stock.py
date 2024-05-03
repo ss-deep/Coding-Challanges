6181ed2a43b24ab61b31da6209428923312553d1
ss-deep <deepalisangole@gmail.com>
2024-05-03 12:46:59 -0500

    maxProfit

---
+++ python/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,43 @@
+'''You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a 
+different day in the future to sell that stock.Return the maximum profit you can achieve from 
+this transaction. If you cannot achieve any profit, return 0.
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.'''
+
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    profit=0
+    min=prices[0]
+    for i in prices:
+        if i<min:
+            min=i
+        elif i>min and profit<(i-min):
+            profit=i-min
+    return profit
+
+print(maxProfit([7,1,5,3,6,4]))
+print(maxProfit([7,6,4,3,1]))
+
+######## OR #########
+    # min = prices[0]
+    # max = prices[0]
+    # for x in prices:
+    #     if x < min:
+    #         min = x
+    #         max = 0
+    #     if x > max:
+    #         max = x
+    #         if max - min > profit:
+    #             profit = max - min
+    # return profit
