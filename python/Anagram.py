c0d57d9d1ed5bcf85c3d89a294bf1349a935d379
ss-deep <deepalisangole@gmail.com>
2024-05-07 11:40:38 -0500

    solved

---
+++ python/Anagram.py
@@ -0,0 +1,14 @@
+'''Given a word w and a text t, Find all starting indices in t which are anagrams of w.
+Example 1:
+Input w = ab
+t = abxaba
+Output [0, 3, 4]
+Example 2:
+Input w = fab
+ t = fabbafafb
+Output [0, 3, 6]
+[INFO] “ Two strings are said to be anagram if they have the exact same characters in different or same sequence.
+Example:
+s1 = “ab” s2 = “ba”
+Frequency of alphabets in s1: a = 1, b = 1 Frequency of alphabets in s2: a = 1, b = 1
+Since frequency of every alphabet is same in both the strings, s1 and s2 are anagrams'''
